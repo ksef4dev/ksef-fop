e56cb7fb40f82971b333badb34a696a2a5e2075c
Adrian Lapierre <al@alapierre.io>
2023-11-12 10:40:09 +0100

    test conf from classpath

--- src/test/java/io/alapierre/ksef/fop/GeneratePdfTest.java
+++ src/test/java/io/alapierre/ksef/fop/GeneratePdfTest.java
@@ -36,6 +36,19 @@ class GeneratePdfTest {
     }
 
     @Test
+    void genWithConfFromClasspath() throws Exception {
+
+        PdfGenerator generator = new PdfGenerator("fop.xconf");
+
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream("src/test/resources/upo.pdf"))) {
+
+            InputStream xml = new FileInputStream("src/test/resources/20231111-SE-E8DDA726E2-F87F056923-EC.xml");
+            Source src = new StreamSource(xml);
+            generator.generateUpo(src, out);
+        }
+    }
+
+    @Test
     void gen() throws Exception {
 
         try (OutputStream out = new BufferedOutputStream(new FileOutputStream("src/test/resources/upo.pdf"))) {
